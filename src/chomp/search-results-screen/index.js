b913fad1f7be4413fa9f87d89576d8dc65095805
Stephen Yu <stephenyu1229@gmail.com>
2023-04-07 15:57:56 -0400

    work on search results screen

--- src/chomp/search-results-screen/index.js
+++ src/chomp/search-results-screen/index.js
@@ -1,7 +1,11 @@
+import React from "react";
 import { useSearchParams } from "react-router-dom";
 import SearchRxs from "../search-rxs";
 import { useEffect, useState } from "react";
 import { findRxs } from "../../services/rxs/rxs-service";
+import "./index.css";
+import RxCard from "./rx-card";
+import { ClipLoader } from "react-spinners";
 
 const SearchResultsScreen = () => {
   const [searchParams, setSearchParams] = useSearchParams();
@@ -9,6 +13,7 @@ const SearchResultsScreen = () => {
   const [results, setResults] = useState([]);
   const [currentIndex, setCurrentIndex] = useState(0);
   const [currentRx, setCurrentRx] = useState({});
+  const [loading, setLoading] = useState()
   const getRxs = async () => {
     const response = await findRxs(searchParamsObject);
     setResults(response);
@@ -26,19 +31,11 @@ const SearchResultsScreen = () => {
   return (
     <>
       <SearchRxs />
-      <div className="mt-3">
-        {currentRx && (
-          <div className="card">
-            <img
-              className="card-img-top"
-              src={currentRx.image_url}
-              alt={currentRx.name}
-            />
-            <div className="card-body">
-              <h5 className="card-title">{currentRx.name}</h5>
-            </div>
-          </div>
-        )}
+      <div className="mt-3 d-flex justify-content-around align-items-center">
+        <button className="btn btn-primary">Previous</button>
+        {console.log(currentRx)}
+        {Object.keys(currentRx).length !== 0 ? <RxCard rx={currentRx} /> : <ClipLoader/>}
+        <button className="btn btn-primary">Next</button>
       </div>
     </>
   );
---
+++ src/chomp/search-results-screen/rx-card.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const RxCard = ({ rx }) => {
+  return (
+    <div className="card">
+      <img className="card-img-top wd-rx-card-img" src={rx.image_url} />
+      <div className="card-body">
+        <h5></h5>
+      </div>
+    </div>
+  );
+};
+
+export default RxCard;
