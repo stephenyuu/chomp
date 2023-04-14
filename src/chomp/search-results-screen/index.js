6d0ec20cc55160d5d2d6759757a8ce24d64cc8c7
Stephen Yu <stephenyu1229@gmail.com>
2023-04-14 15:56:22 -0400

    delete unnecessary state variables

--- src/chomp/search-results-screen/index.js
+++ src/chomp/search-results-screen/index.js
@@ -10,27 +10,20 @@ const SearchResultsScreen = () => {
   const [searchParams, setSearchParams] = useSearchParams();
   const searchParamsObject = Object.fromEntries(searchParams.entries());
   const [results, setResults] = useState([]);
-  const [currentIndex, setCurrentIndex] = useState(0);
-  const [currentRx, setCurrentRx] = useState({});
   const getRxs = async () => {
     const response = await findRxs(searchParamsObject);
     setResults(response);
   };
   useEffect(() => {
-    setCurrentRx({});
+    setResults({});
     getRxs();
   }, [searchParams]);
-  useEffect(() => {
-    if (results.length > 0) {
-      setCurrentRx(results[currentIndex]);
-    }
-  }, [results, currentIndex]);
   return (
     <>
       <SearchRxs />
       <h1 className="mt-3 mb-0 fw-bold">Restaurants</h1>
       <div className="mt-3">
-        {Object.keys(currentRx).length !== 0 ? (
+        {Object.keys(results).length !== 0 ? (
           <SearchResultsCarousel rxs={results} />
         ) : (
           <LoadingBar />
