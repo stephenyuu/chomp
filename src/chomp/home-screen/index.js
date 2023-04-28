0f4bc5a0dc19001c876e6acc34fff8ce1192040d
Stephen Yu <stephenyu1229@gmail.com>
2023-04-27 21:11:04 -0400

    change welcome message

--- src/chomp/home-screen/index.js
+++ src/chomp/home-screen/index.js
@@ -2,11 +2,9 @@ import React from "react";
 import Chomp from "..";
 import { useSelector } from "react-redux";
 import { useNavigate } from "react-router";
-import "./index.css"
-
+import "./index.css";
 
 const HomeScreen = () => {
-
   const { currentUser } = useSelector((state) => state.users);
   const navigate = useNavigate();
 
@@ -18,8 +16,18 @@ const HomeScreen = () => {
     <Chomp className="" activeLink="">
       <div className="wd-home-container">
         <h1 className="wd-home-title">CHOMP</h1>
-        {currentUser ? (<h2>Welcome {currentUser.firstName}!</h2>) : (<h2>Welcome!</h2>)}
-        <p className="wd-intro mt-1 mb-3">Discover your next favorite restaurant with Chomp - the user-friendly web app for browsing and exploring dining spots. Favorite and like the ones that catch your eye, or become a registered reviewer and share your food experiences with other foodies. Start exploring culinary delights today with Chomping Below!</p>
+        {currentUser ? (
+          <h2>Welcome {currentUser.firstName}!</h2>
+        ) : (
+          <h2>Welcome!</h2>
+        )}
+        <p className="wd-intro mt-1 mb-3">
+          Discover your next favorite restaurant with Chomp - the user-friendly
+          web app for browsing and exploring dining spots. Favorite the ones
+          that catch your eye, or become a Nibbler and share your food
+          experiences with others. Start exploring restaurant scene like never
+          before!
+        </p>
         <button
           className="wd-home-button"
           type="button"
